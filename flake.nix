5dad18f8044a9f35b197d6837da37d1e7685f4f2
brokenpip3 <brokenpip3@gmail.com>
2025-04-25 16:39:35 +0200

    update to python 3.13

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "LOTB: Lord of telegram bots";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
@@ -10,8 +10,15 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      poetry2nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication defaultPoetryOverrides;
@@ -24,48 +31,48 @@
 
         packages = {
           "${lotbName}" = mkPoetryApplication {
-            python = pkgs.python312;
+            python = pkgs.python313;
             projectDir = self;
-            checkPhase = "pytest -s -v -k 'not unsplash'";
+            checkPhase = ''
+              runHook preCheck
+              pytest -s -v -k 'not unsplash'
+              runHook postCheck
+            '';
             # Apparently this is not working
             disabledTests = [
               "test_unsplash_search_success"
               "test_unsplash_search_no_results"
               "test_unsplash_search_api_error"
             ];
-            overrides = defaultPoetryOverrides.extend
-              (final: prev: {
-                python-telegram-bot = prev.python-telegram-bot.overridePythonAttrs
-                  (
-                    old: {
-                      buildInputs = (old.buildInputs or [ ]) ++ [ prev.hatchling ];
-                    }
-                  );
-              });
+            overrides = defaultPoetryOverrides.extend (
+              final: prev: {
+                python-telegram-bot = prev.python-telegram-bot.overridePythonAttrs (old: {
+                  buildInputs = (old.buildInputs or [ ]) ++ [ prev.hatchling ];
+                });
+              }
+            );
           };
           default = self.packages.${system}."${lotbName}";
         };
 
-        packages.dockerimg = pkgs.dockerTools.buildImage
-          {
-            name = "${dockerRegistry}/${lotbName}";
-            tag = "${lotbVersion}";
-            created = "now";
-            copyToRoot = self.packages.${system}.default;
-            config = {
-              Labels = {
-                "maintainer" = "brokenpip3";
-                "description" = "Docker image for ${lotbName}";
-                "version" = "${lotbVersion}";
-                "org.opencontainers.image.authors" = "brokenpip3 <brokenpip3@gmail.com>";
-                "org.opencontainers.image.title" = "lotb";
-                "org.opencontainers.image.description" = "Lord of telegram bots";
-                "org.opencontainers.image.url" = "ghcr.io/brokenpip3/lotb";
-                "org.opencontainers.image.source" = "https://github.com/brokenpip3/lotb";
-              };
+        packages.dockerimg = pkgs.dockerTools.buildImage {
+          name = "${dockerRegistry}/${lotbName}";
+          tag = "${lotbVersion}";
+          created = "now";
+          copyToRoot = self.packages.${system}.default;
+          config = {
+            Labels = {
+              "maintainer" = "brokenpip3";
+              "description" = "Docker image for ${lotbName}";
+              "version" = "${lotbVersion}";
+              "org.opencontainers.image.authors" = "brokenpip3 <brokenpip3@gmail.com>";
+              "org.opencontainers.image.title" = "lotb";
+              "org.opencontainers.image.description" = "Lord of telegram bots";
+              "org.opencontainers.image.url" = "ghcr.io/brokenpip3/lotb";
+              "org.opencontainers.image.source" = "https://github.com/brokenpip3/lotb";
             };
           };
-
+        };
 
         devShells.default = pkgs.mkShell {
           inputsFrom = [ self.packages.${system}.lotb ];
@@ -82,5 +89,6 @@
           PYTHONDONTWRITEBYTECODE = 1;
           POETRY_VIRTUALENVS_IN_PROJECT = 1;
         };
-      });
+      }
+    );
 }
